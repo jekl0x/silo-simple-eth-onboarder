616a0c6ef8c499a4203f119dbd408c0f9960cde3
jekl <102255715+jekl0x@users.noreply.github.com>
2023-07-18 22:29:33 -0400

    Update targetDao.ts

--- src/targetDao.ts
+++ src/targetDao.ts
@@ -20,14 +20,14 @@ export const TARGETS: {
     STAKE_NEXT_START: number;
 
 } = {
-    NAME: "NOW DAO",
+    NAME: "SILO DAO",
     DAO_ADDRESS: "0x57fbfaa786b92bde7db05f10f667c173076ce5c8",
     SAFE_ADDRESS: "0x77f5b5f1eba2dc047657f6fd91e9618565f72f25",
     CHAIN_ID: "0x5",
     SHARE_ADDRESS: "0xbf1cae67d4a42946dd04b0d375c1c5c999018f1e",
-    SHARE_SYMBOL: "vNOW",
+    SHARE_SYMBOL: "vSILO",
     LOOT_ADDRESS: "0x271dd85a29233386f26e032419c8cbb9074ab8f1",
-    LOOT_SYMBOL: "NOW",
+    LOOT_SYMBOL: "SILO",
     STAKE_TOKEN_NAME: "ETH",
     STAKE_TOKEN_SYMBOL: "ETH",
     STAKE_TOKEN_DECIMALS: 18,
@@ -39,10 +39,10 @@ export const TARGETS: {
 };
 
 export const ABOUTLINKS = {
-  discord: "https://discord.gg/daohaus",
-  twitter: "https://twitter.com/nowdaoit",
-  github: "https://github.com/HausDAO/",
-  githubOnboarder: "https://github.com/HausDAO/publichaus",
-  logo_url: "https://yeet.daohaus.club/static/media/y33t_logo.67335638.svg",
-  hero: "https://yeet.daohaus.club/static/media/yeet-pirate.e556464d.gif"
+  discord: "https://discord.gg/QpaXn6CFAN",
+  twitter: "https://twitter.com/Silo_Haus",
+  github: "https://github.com/SiloHaus/",
+  githubOnboarder: "https://github.com/silohaus/simple-eth-yeeter",
+  logo_url: "https://media.discordapp.net/attachments/1126954803847770122/1131050067642564639/Silo_Logo_02_Ring.png",
+  hero: "https://media.discordapp.net/attachments/839168503927799808/1130942613248282796/SiloHaus_social.png?width=810&height=425"
 };
